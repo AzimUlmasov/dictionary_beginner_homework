80fb4c20685f9f725025d1c64bec7b81acfedebb
Azim Ulmasov <azimulmasov2@gmail.com>
2023-01-23 21:34:05 +0500

    11

--- get_user_names_with_age.py
+++ get_user_names_with_age.py
@@ -12,17 +12,3 @@ def get_user_names_with_age(data:list, age:int) -> list:
     for i in data:
         if age == i["age"]:
             return i["name"]
-
-data = [
-  {
-    'name': 'John', 
-    'age': 27
-  }, 
-  {
-    'name': 'Mary', 
-    'age': 42
-  }
-]
-age = 27
-
-print(get_user_names_with_age(data, age))
