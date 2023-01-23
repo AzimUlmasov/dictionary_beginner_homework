a8fb0ef5478be333240dad2b4c626175673abc32
AzimUlmasov <azimulmasov2@gmail.com>
2023-01-23 11:16:20 +0500

    02

--- count_users_with_age.py
+++ count_users_with_age.py
@@ -13,18 +13,3 @@ def count_users_with_age(data:list, age:int) -> int:
         if ages['age'] == age:
             count += 1
     return count
-
-# data = [
-#   {
-#     'name': 'John', 
-#     'age': 35
-#   },
-#   {
-#     'name':'Mary', 
-#     'age': 20
-#   }
-#   ]
-
-# age = 38
-
-# print(count_users_with_age(data, age))
